b9865b308f3f0b14f363a5dd38e2fcb6f40de265
billow <billow.fun@gmail.com>
2024-12-03 06:02:13 +0800

    add sitemap and tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,31 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import cloudflare from '@astrojs/cloudflare';
+
+import sitemap from '@astrojs/sitemap';
+
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://sbc.hoshinoaya.workers.dev',
+  output: 'server',
+
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true
+    }
+  }),
+
+  integrations: [sitemap({
+    i18n: {
+      defaultLocale: 'en', // All urls that don't contain `es` or `fr` after `https://stargazers.club/` will be treated as default locale, i.e. `en`
+      locales: {
+        en: 'en-US', // The `defaultLocale` value must present in `locales` keys
+        es: 'es-ES',
+        zh: 'zh-CN',
+      },
+    },
+  }), tailwind()]
+});
